917f5a6486f4f16a1244b11a45372ee041eb4cb5
brenosekiro <b88660501@gmail.com>
2020-09-25 20:11:45 -0300

    prismaSetup

---
+++ backend/prisma/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE "public"."User" (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    avatar VARCHAR(255) NOT NULL DEFAULT 'default.png'
+);
+
+CREATE TABLE "public"."Playlist" (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    FOREIGN KEY ("userId") REFERENCES "public"."User"(id)
+);
+
+CREATE TABLE "public"."Music" (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    avatar VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "public"."PlaylistToMusic" (
+    "playlistId" INTEGER NOT NULL,
+    "musicId" INTEGER NOT NULL,
+    FOREIGN KEY ("playlistId") REFERENCES "public"."Playlist"(id),
+    FOREIGN KEY ("musicId") REFERENCES "public"."Music"(id)
+);
+
+CREATE UNIQUE INDEX "PlaylistToMusic_index" ON "public"."PlaylistToMusic"("playlistId" int4_ops, "musicId" int4_ops);
