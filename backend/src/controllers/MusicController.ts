fe1340402a733a68370c1c401e9c8cd3789c8532
brenosekiro <b88660501@gmail.com>
2020-09-26 10:26:05 -0300

    musicController

---
+++ backend/src/controllers/MusicController.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient
+
+class MusicController {
+    
+    async createMusic(req: Request, res: Response) {
+
+        const { name, author, avatar, url } = req.body
+
+        const music = await prisma.music.create({
+            data: {
+                name, author, avatar, url, PlaylistToMusic: {
+                    create: {
+                        Playlist: {
+                            connect: {
+                                id: 1
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        return res.status(201).json(music)
+
+    }
+
+    async getMusicByPlaylist(req: Request, res: Response) {
+
+        const { id } = req.params
+
+        const data = await prisma.playlist.findOne({
+            where: {
+                id: Number(id)
+            },
+            include: {
+                PlaylistToMusic: {
+                    include: {
+                        Music: true
+                    }
+                }
+            }
+        })
+
+        return res.status(201).json(data)
+    }
+
+}
+
+export default MusicController
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express'
+import MusicController from './controllers/MusicController'
 import PlaylistController from './controllers/PlaylistController'
 import UserController from './controllers/UserController'
 const routes = Router()
 
 const userController = new UserController()
 const playlistController = new PlaylistController()
+const musicController = new MusicController()
 
 routes.post('/user/create', userController.createUser)
 routes.post('/user/auth', userController.authUser)
 routes.post('/playlist/create', playlistController.createPlaylist)
 routes.get('/playlist/user/:id', playlistController.getPlaylistsById)
+routes.post('/music/create', musicController.createMusic)
+routes.get('/music/playlist/:id', musicController.getMusicByPlaylist)
 
 export default routes
