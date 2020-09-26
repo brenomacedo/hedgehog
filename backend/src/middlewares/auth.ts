f39627172d4ac10095aca1e225c5cb069db8de91
brenosekiro <b88660501@gmail.com>
2020-09-26 10:40:55 -0300

    authenticationMiddleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express'
+import { key } from '../secret.json'
+import jwt, { DecodeOptions } from 'jsonwebtoken'
+
+const AuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers.authorization
+    if(!header) {
+        return res.status(500).json({ error: "Token not provided!" })
+    }
+
+    const headerStringArray = header.split(' ')
+
+    if(headerStringArray.length !== 2) {
+        return res.status(500).json({ error: "Invalid token format!" })
+    }
+
+    if(headerStringArray[0] === 'Bearer') {
+        return res.status(500).json({ error: "Invalid token format!" })
+    }
+
+    jwt.verify(headerStringArray[1], key, (err, decoded: any) => {
+        if(err) {
+            return res.status(500).json({ error: "Invalid token!" })
+        }
+
+        req.body.userId = decoded.id
+    })
+
+    next()
+}
+
+export default AuthMiddleware
